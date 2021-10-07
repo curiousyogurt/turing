b8cf67972fc3282a4c4dd789d5e24b7393f321db
Darcy <darcyotto@gmail.com>
2021-10-07 11:10:07 -0700

    Fixed info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 
 (define collection "turing")
-(define name "turing")
 (define version "1.0")
 (define pkg-authors '(Darcy Otto))
 (define pkg-desc "Turing language for Racket, modelled on Alan Turing's 1936 paper.")
 
+(define deps '("base"))
 (define scribblings '(("scribblings/turing.scrbl" (multi-page))))
