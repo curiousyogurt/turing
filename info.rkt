56414e4532abe6f307fab2fe2360ff2c884df163
Darcy <darcyotto@gmail.com>
2021-10-09 18:20:57 -0700

    Added dependencies

--- info.rkt
+++ info.rkt
@@ -5,5 +5,5 @@
 (define pkg-authors '(Darcy Otto))
 (define pkg-desc "Turing language for Racket, modelled on Alan Turing's 1936 paper.")
 
-(define deps '("base"))
+(define deps '("base" "racket-doc" "scribble-lib"))
 (define scribblings '(("scribblings/turing.scrbl" ())))
